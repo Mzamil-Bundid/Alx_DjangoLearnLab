db1bde29c211dc5ecb283a44fef23a47dd19315f
Mzamil Bundid <mzamil.bundid@gmail.com>
2025-07-27 19:49:24 +0300

    implementing custom permisions in django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -26,6 +26,11 @@ class Book(models.Model):
     
     class Meta:
         ordering = ['title']
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
 
 class Library(models.Model):
