e866873c2f3e4f8f38e0f3361eecf40622a348a3
Mzamil Bundid <mzamil.bundid@gmail.com>
2025-07-27 18:34:51 +0300

    updated views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library  # Ensure Library is imported
+from .models import Book
+from .models import Library
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
